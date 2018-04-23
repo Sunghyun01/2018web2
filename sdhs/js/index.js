62d2359d6ec18bc9cb68b6c979ce8104026ace29
Sunghyun01 <37784262+Sunghyun01@users.noreply.github.com>
2018-04-23 13:21:51 +0900

    학교 홈페이지

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -1,9 +1,28 @@
+function up() {
+    var td = document.getElementById("test");
+    var n = Number(td.innerHTML);
+    if(n>=30){
+        n=30;
+    }else{
+        td.innerHTML = n + 1;
+    }
+    td.innerHTML = n + 1;
+}
+function down() {
+    var td = document.getElementById("test");
+    var n = Number(td.innerHTML);
+    if(n<=1){
+         n=1;
+    }else{
+        td.innerHTML = n - 1;
+    }
+
+}
 $(document).ready(function () {
     var slide=$("#slide>li");
     var i=1;
     setInterval(function(){
         slide.eq(i).css("left","1000px");
-
         slide.eq(i-1).animate({"left":"-1000px"},2000);
         slide.eq(i).animate({"left":"0"},2000)
         if(i<3)
@@ -12,3 +31,4 @@ $(document).ready(function () {
             i=0;
     },2000);
 })
+
